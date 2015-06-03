60c3c14da05f5fdac693994cbb1149637484f653
PikaPhu <whitehunter100@gmail.com>
2015-06-03 10:51:33 +0700

    "Add UnitTest"

---
+++ C#/MyTestUnit/MyTestUnit/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyTestUnit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+
+        [TestMethod]
+        public void TestMethod2(int val1)
+        {
+            if (val1 < 0)
+            {
+                throw new ArgumentException("value cannot less than zero");
+            }
+        }
+    }
+}
