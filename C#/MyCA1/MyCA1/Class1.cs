88c2d55d3925444060aea17688b619a87c2b3e0e
PikaPhu <whitehunter100@gmail.com>
2015-06-03 11:34:48 +0700

    Add MyCA1 and TestSQLite

---
+++ C#/MyCA1/MyCA1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCA1
+{
+    class Class1 : TestEvent
+    {
+        public Class1() : base()
+        {
+            Console.WriteLine("Called from Default Constr");
+        }
+
+        public Class1(string arg) : this()
+        {
+            Console.WriteLine("Called from Constr1");
+        }
+
+        public Class1(string arg1, int arg2) : this("Constr2")
+        {
+            Console.WriteLine("Called from Constr2");
+        }
+    }
+}
